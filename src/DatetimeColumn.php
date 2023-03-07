f3487e35fa87d55b335f8d6f4959ef05739b0a18
alexiss-k <alexiskravchenko@gmail.com>
2023-03-07 16:56:42 +0200

    Update DatetimeColumn.php

--- src/DatetimeColumn.php
+++ src/DatetimeColumn.php
@@ -17,9 +17,19 @@ class DatetimeColumn extends Column
     public function format($format = null)
     {
         $this->callback = function ($value) use ($format) {
+            if ($value && $this->datetimeFormatInternal === 'U') {
+                return Carbon::createFromTimestamp($value)->format($format ?? config('livewire-datatables.default_datetime_format'));
+            }
+
             return $value ? Carbon::parse($value)->format($format ?? config('livewire-datatables.default_datetime_format')) : null;
         };
 
+        return $this;
+    }
+
+    public function datetimeFormatInternal($format)
+    {
+        $this->datetimeFormatInternal = $format;
         return $this;
     }
 }
